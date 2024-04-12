f7e5b2e3c08b16af712ca2aabfb4965e8898ba18
anton <anton@DESKTOP-5L5HEP2>
2024-04-12 11:15:32 +0300

    Добавьте файлы проекта.

---
+++ Library/Library.cs
@@ -0,0 +1,19 @@
+﻿namespace MyLibrary
+{
+    public class Library
+    {
+        public static double AreaCalculation(double r)
+        {
+            return Math.PI * r * r;
+        }
+        public static double AreaCalculation(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        public static bool CheckingSquareness(double a, double b, double c)
+        {
+            return a * a + b * b == c * c || a * a + c * c == b * b || c * c + b * b == a * a;
+        }
+    }
+}
---
+++ LibraryTests/LibraryTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Tests
+{
+    [TestClass()]
+    public class LibraryTests
+    {
+        [TestMethod()]
+        public void AreaCalculationСircleTest()
+        {
+            double r = 1;
+            double expected = Math.PI;
+
+            double actual = Library.AreaCalculation(r);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void AreaCalculationTriangleTest()
+        {
+            double a = 3;
+            double b = 4;
+            double c = 5;
+            double expected = 6;
+
+            double actual = Library.AreaCalculation(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void CheckingSquarenessTest()
+        {
+            double a = 3;
+            double b = 4;
+            double c = 5;
+            bool expected = true;
+
+            bool actual = Library.CheckingSquareness(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
